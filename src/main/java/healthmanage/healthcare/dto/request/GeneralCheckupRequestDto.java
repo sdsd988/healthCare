39a1fc1c82e893114611d13f5e5f9d6cb9e46eb8
JUNGSANGYUN <sdsd98987@gmail.com>
2025-12-12 15:45:08 +0900

    1. 리스트화면에서 상세 화면으로 넘어가는 api 수정 2. 생성화면 개발

--- src/main/java/healthmanage/healthcare/dto/request/GeneralCheckupRequestDto.java
+++ src/main/java/healthmanage/healthcare/dto/request/GeneralCheckupRequestDto.java
@@ -7,20 +7,38 @@ import lombok.Setter;
 @Setter
 public class GeneralCheckupRequestDto {
 
-  // 기본 정보
-  private String workerName;          // 직원명
-  private String department;          // 부서
-  private String occupation;          // 직무
+    // 기본 정보
+    String workerName;          // 직원명
+    String department;          // 부서
+    String occupation;          // 직무
+    String birthDate;           // 생년월일
+    String gender;              // 성별
+    String workType;            // 근무형태
 
-  // 일반건강검진 결과
-  private String generalCheckupGrade; // 종합등급 (A, B, C, D)
-  private String bloodPressure;       // 혈압(mmHg)
-  private String bloodSugar;          // 혈당
-  private String cholesterol;         // 콜레스테롤
-  private String findings;            // 유소견 사항
 
-  // 사후관리
-  private String workFit;             // 작업 적합성
-  private Boolean hasHealthConsultation; // 보건상담 여부 (true/false)
-  private String doctorOpinion;       // 의사 소견
+    // 일반건강검진 결과
+    String highBloodPressureGrade;  // 혈압
+    String diastolic;               // 이완기혈압수치
+    String systolic;                // 수축기혈압수치
+    String lipidGrade;              // 이상지질
+    String totalCholesterol;        // 총콜레스테롤
+    String triglyceride;            // 중성지방
+    String hdl;                     // HDL
+    String ldl;                     // LDL
+    String kidneyDiseaseGrade;      // 신장질환등급
+    String urineProtein;            // 요단백
+    String gfr;                     // 사구체여과율
+    String creatinine;              // 크레아틴
+    String obesityGrade;            // 비만등급
+    String waist;                   // 허리둘레
+    String bmi;                     // 체질량지수
+    String smokingYn;               // 흡연여부
+    String drunkenYn;               // 음주여부
+    String fastingBloodSugar;       // 공복혈당
+    String examYear;                // 검진년도
+    String diabetesGrade;           // 당뇨
+    String liverDiseaseGrade;        // 간질환등급
+    String ast;                     // AST
+    String alt;                     // ALT
+    String gtp;                     // GTP
 }
