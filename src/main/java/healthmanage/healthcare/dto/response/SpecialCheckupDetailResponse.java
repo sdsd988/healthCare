366441088c51111ad142e8cfcf8363526d2c5a39
JUNGSANGYUN <sdsd98987@gmail.com>
2025-12-12 17:59:24 +0900

    특수건강검진 DTO 개발

--- src/main/java/healthmanage/healthcare/dto/response/SpecialCheckupDetailResponse.java
+++ src/main/java/healthmanage/healthcare/dto/response/SpecialCheckupDetailResponse.java
@@ -1,13 +1,10 @@
 package healthmanage.healthcare.dto.response;
 
 public class SpecialCheckupDetailResponse {
-    //개별판정등급
 
-    //조혈기계등급
-
-    //신경계등급
-
-    //위장장애등급
-
+    String individualJudgmentGrade; //개별판정등급
+    String hematopoieticSystemGrade; //조혈기계등급
+    String nervousSystemGrade; //신경계등급
+    String gastrointestinalDisorderGrade; //위장장애등급
     String examYear; //검진년도
 }
